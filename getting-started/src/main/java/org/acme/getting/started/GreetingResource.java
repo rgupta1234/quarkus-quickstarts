5b0d18884a031b8a7f724b2196d0f107b89c4698
rgupta1234 <rkgupta75@gmail.com>
2020-08-09 20:02:40 -0500

    Update GreetingResource.java

--- getting-started/src/main/java/org/acme/getting/started/GreetingResource.java
+++ getting-started/src/main/java/org/acme/getting/started/GreetingResource.java
@@ -5,6 +5,10 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Gauge;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
